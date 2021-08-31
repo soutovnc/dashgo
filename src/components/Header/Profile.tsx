dd65e0f48aa3cf008a6243e91553fcfadb0fbff5
Vinicius <viniciusdesouto@gmail.com>
2021-08-31 20:11:43 -0300

    add responsivity in Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Vinícius de Souto</Text>
-        <Text color="gray.300" fontSize="small">viniciusdesouto@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Vinícius de Souto</Text>
+          <Text color="gray.300" fontSize="small">viniciusdesouto@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vinicius de Souto" src="https://github.com/soutovnc.png" />
     </Flex>
