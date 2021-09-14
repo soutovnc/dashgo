9fd69ca233be8105a58e57b819b1cfab86c56043
Vinicius <viniciusdesouto@gmail.com>
2021-09-13 23:19:13 -0300

    add faker, and config settings for any data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,19 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useEffect } from "react";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar/index'
 
 export default function UserList() {
+  useEffect(() => {
+    fetch('https://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
