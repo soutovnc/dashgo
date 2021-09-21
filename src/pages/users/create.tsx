4ec74d218f0d24fe9b56df02a4f334401759b3b3
Vinicius <viniciusdesouto@gmail.com>
2021-09-20 23:15:58 -0300

    add useMutation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,9 @@ import { Header } from "../../components/Header";
 import { Sidebar } from '../../components/Sidebar/index'
 import { Input } from '../../components/Form/Input'
 
+import { useMutation } from 'react-query'
+import { api } from '../../services/api'
+
 type CreateUserFormData = {
   name: string;
   email: string;
@@ -25,6 +28,17 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user
+  })
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
@@ -32,9 +46,7 @@ export default function CreateUser() {
   const { errors } = formState
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-
-    console.log(values)
+    await createUser.mutateAsync(values)
   }
 
   return (
